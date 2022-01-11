95cfa24d4cf673101cf0ed8f90fe78e1e8b7ef2d
mohitsharma98 <mohitsharma30898@gmail.com>
2022-01-11 12:39:44 +0530

    added your name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Mohit - How are you?
+          This is Ridhima - How are you?
         </p>
         <a
           className="App-link"
